9bb4ffee3bd5b0ffd1f47fd5ad0fbbd18fa519dd
Viplav Nagpal <viplavnagpal1704@gmail.com>
2026-05-29 07:57:37 -0400

    Solution to Longest-Substring question in python

--- Python-Answers/Add-Two-Numbers.py
+++ Python-Answers/Add-Two-Numbers.py
@@ -1,8 +1,12 @@
+from typing import Optional
+
 # Definition for singly-linked list.
 class ListNode:
-     def __init__(self, val=0, next=None):
+    def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+
+# Solution to add two numbers represented as linked lists
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         solution = ListNode()
@@ -23,7 +27,4 @@ class Solution:
             l1 = l1.next if l1 else None
             l2 = l2.next if l2 else None
                 
-
-        return (solution.next)
-
-        
+        return solution.next
