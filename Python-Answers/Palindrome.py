b5f4c76dfb924721f00d886536f28d57d9b230b6
Viplav Nagpal <viplavnagpal1704@gmail.com>
2026-06-03 13:40:35 -0400

    Solved Roman to Integer and fixed files name of Longest-Substring

--- Python-Answers/Palindrome.py
+++ Python-Answers/Palindrome.py
@@ -1,21 +1,16 @@
-x = 10
-
-if x < 0:
-    print(False)
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        array = list(map(int, str(x)))
         
-array = list(map(int, str(x)))
-n = len(array)
-mid = n // 2
+        n = len(array)
+        mid = n // 2
 
-count = 2 if n % 2 != 0 else 1
+        count = 2 if n % 2 != 0 else 1
 
-for i in range(mid+count-1, n):
-    if array[i] != array[i-count]:
-        print(False)
-        break
-    count += 2
-
-        
-        
-
-print(mid)
+        for i in range(mid+count-1, n):
+            if array[i] != array[i-count]:
+                return False
+            count += 2
+        return True
