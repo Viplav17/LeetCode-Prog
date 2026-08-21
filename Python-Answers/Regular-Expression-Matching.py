829ced1658cc82375e71293d929b910e447bfa3f
nagpalvv <nagpalvv@mail.uc.edu>
2026-08-21 06:16:38 +0530

    Added cases for * in the pattern. Starting debugging to test

--- Python-Answers/Regular-Expression-Matching.py
+++ Python-Answers/Regular-Expression-Matching.py
@@ -1,16 +1,26 @@
 s = "aa"
-p = "a"
+p = "a*"
 
 n_s = len(s)
 n_p = len(p)
 
 pattern = ""
 index = 0
+last_char = ""
 
 while len(pattern) <= n_s:
     if p[index] is None:
         print(False)
         break
+
+    if p[index].isalpha():
+        last_char = p[index]
+        pattern += p[index]
+        index += 1
+        continue
+    
+    if p[index] == "*":
+        pattern += last_char
 
     
     
