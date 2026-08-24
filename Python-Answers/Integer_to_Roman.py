92e175bed3c9dcda629be01c4c16494af98928a9
nagpalvv <nagpalvv@mail.uc.edu>
2026-08-24 18:38:13 +0530

    Created two functions to return the roman equivalent of the input number. Two seperate functions where one of them is specificially for numbers starting with 4 or 9

--- Python-Answers/Integer_to_Roman.py
+++ Python-Answers/Integer_to_Roman.py
@@ -3,13 +3,48 @@ num = 1994
 Roman_Map = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M":1000}
 solution = ""
 change = (10 ** (len(str(num)) - 1))
+
+
+
+def give_Roman(num):
+    solution = ""
+    track = "I"
+
+    while num != 0:
+        for Roman, Value in Roman_Map.items():
+            if num < Value:
+                solution += track
+                num -= Roman_Map[track]
+                track = ""
+                break
+            elif Value == 1000:
+                solution += Roman
+                num -= 1000
+                track = ""
+                break
+            else:
+                track = Roman
+                continue
+
+    return solution
+
+def give_Roman_4(num):
+    solution = ""
+    track = "I"
+
+    while num != 0:
+        for Roman, Value in Roman_Map.items():
+            if num <= Value:
+                num = Value - num
+                solution = Roman + solution
+                break
+
+    return solution
                 
 while num > 0:
     digit = num // change
     digit_value = digit * change
 
- 
-    num = num % change
     change /= 10
 
 print(solution)
