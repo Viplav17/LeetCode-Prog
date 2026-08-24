2cf80b49758b3b0a147e42250f90c006a5baeac3
nagpalvv <nagpalvv@mail.uc.edu>
2026-08-24 18:35:46 +0530

    Created a dict of all roman numbers and their corresponding values and added a variable called change

--- Python-Answers/Regular-Expression-Matching.py
+++ Python-Answers/Regular-Expression-Matching.py
@@ -1,35 +1,3 @@
 s = "ab"
-p = ".*"
 
-n_s = len(s)
-n_p = len(p)
-
-pattern = ""
-index = 0
-last_char = ""
-
-while len(pattern) < n_s and index < n_p:
-    if p[index] is None:
-        print(False)
-        break
-
-    if p[index].isalpha():
-        last_char = p[index]
-        pattern += p[index]
-        index += 1
-        continue
-    
-    if p[index] == "*":
-        pattern += last_char
-    elif p[index] == ".":
-        pattern += s[index]
-        last_char = s[index]
-        index += 1
-
-
-if pattern == s:
-    print(True)
-
-else:
-    print(False)
-    
+  
